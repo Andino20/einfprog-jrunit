6153d9cc0689808b88791f1ae40c8f401856f6ab
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2026-04-20 10:59:37 +0200

    Add tests to examples

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -7,5 +7,12 @@ repositories {
 }
 
 dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation(project(":lib"))
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
